db58e05b52bdcffdc42a172b905d78ecb00e0f67
Samyak Shah <samyakshah3008@gmail.com>
2024-04-24 22:38:14 +0530

    middleware implementations

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 
 const app = express();
 
-app.use(cors);
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.post("/api/hello", (req, res) => {
   res.send({ msg: "hello world" });
