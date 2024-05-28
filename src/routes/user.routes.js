a12aa2ee21505d2c17d46d298756c8731a6a05b3
Samyak Shah <samyakshah3008@gmail.com>
2024-05-28 16:43:41 +0530

    feat: verify otp route add

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,16 +4,18 @@ import {
   logoutUser,
   refreshAccessToken,
   registerUser,
+  verifyOTP,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
-
 router.route("/login").post(loginUser);
+router.route("/verify-otp").post(verifyOTP);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
+router.route("/refresh-access-token").post(refreshAccessToken);
+
 export default router;
