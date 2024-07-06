06b565a3e3c6e6ebed9c6aa164352bd7e884e4bc
Samyak Shah <samyakshah3008@gmail.com>
2024-07-07 00:40:28 +0530

    cookie options constant add

--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,8 @@ export const generateOTP = () => {
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
   return otp;
 };
+
+export const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+};
