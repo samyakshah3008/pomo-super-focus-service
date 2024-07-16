cd44e9b90125ca3c7004a777f74a6ac3dcfeeeb4
Samyak Shah <samyakshah3008@gmail.com>
2024-07-16 13:43:59 +0530

    feat: pomodoro controllers add

---
+++ src/controllers/pomodoros/pomodoro.controller.js
@@ -0,0 +1,44 @@
+import {
+  getPomodoroReportService,
+  logPomodoroSessionService,
+} from "../../services/pomodoro/pomodoroService.js";
+import { ApiError } from "../../utils/apiError.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+
+const logPomodoroSession = asyncHandler(async (req, res) => {
+  const { userId, sessionTime } = req.body;
+
+  try {
+    const response = await logPomodoroSessionService(userId, sessionTime);
+    return res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json(error);
+    }
+    return res
+      .status(500)
+      .json(
+        new ApiError(
+          500,
+          error,
+          "something went wrong while logging pomodoro session"
+        )
+      );
+  }
+});
+
+const getPomodoroReport = asyncHandler(async (req, res) => {
+  const { userId, startDate, endDate } = req.query;
+
+  try {
+    const response = await getPomodoroReportService(userId, startDate, endDate);
+    return res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      return res.json(error);
+    }
+    return res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+export { getPomodoroReport, logPomodoroSession };
