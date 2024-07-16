089d0007545b7326ec643f8b92321069d5f4da42
Samyak Shah <samyakshah3008@gmail.com>
2024-07-16 13:45:35 +0530

    feat: routes add for pomodoro

--- src/routes/pomodoro.routes.js
+++ src/routes/pomodoro.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import {
+  getPomodoroReport,
+  logPomodoroSession,
+} from "../controllers/pomodoros/pomodoro.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", verifyJWT);
+router.route("/log-session").post(verifyJWT, logPomodoroSession);
+router.route("/report").get(verifyJWT, getPomodoroReport);
 
 export default router;
