0a3d7350628917397e915e756e87b6065da30d91
Samyak Shah <samyakshah3008@gmail.com>
2024-07-24 00:30:22 +0530

    feat: wiring of active pomodoro with routes

--- src/routes/pomodoro.routes.js
+++ src/routes/pomodoro.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import {
+  deleteActivePomodoro,
+  initializePomodoro,
+  pauseOrResumePomodoro,
+} from "../controllers/pomodoros/activePomodoro.controller.js";
+import {
   getPomodoroReport,
   logPomodoroSession,
 } from "../controllers/pomodoros/pomodoro.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.route("/active-pomodoro").post(verifyJWT, initializePomodoro);
+router.route("/active-pomodoro").patch(verifyJWT, pauseOrResumePomodoro);
+router.route("/active-pomodoro").delete(verifyJWT, deleteActivePomodoro);
 
 router.route("/log-session").post(verifyJWT, logPomodoroSession);
 router.route("/report").get(verifyJWT, getPomodoroReport);
