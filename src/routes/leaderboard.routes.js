3bfa7c2324d4168f877b9a2d1c4ba107153e1e30
Samyak Shah <samyakshah3008@gmail.com>
2024-10-03 18:04:23 +0530

    feat: leaderboard controller functions add

--- src/routes/leaderboard.routes.js
+++ src/routes/leaderboard.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
-import { getLeaderboardOfTheWeek } from "../controllers/leaderboard/leaderboard.controller.js";
+import {
+  getLeaderboardOfTheWeek,
+  getUserRankOfTheWeek,
+} from "../controllers/leaderboard/leaderboard.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/weekly").get(verifyJWT, getLeaderboardOfTheWeek);
+router.route("/weekly/:id").get(verifyJWT, getUserRankOfTheWeek);
 
 export default router;
