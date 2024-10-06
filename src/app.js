ec13e38f482227667a8d7b61aa4b94aa8a354bdd
Samyak Shah <samyakshah3008@gmail.com>
2024-10-06 16:46:48 +0530

    feat: working framework route add

--- src/app.js
+++ src/app.js
@@ -15,11 +15,13 @@ import pomodoroRouter from "./routes/pomodoro.routes.js";
 import streakRouter from "./routes/streak.routes.js";
 import todoRouter from "./routes/todo.routes.js";
 import userRouter from "./routes/user.routes.js";
+import workingFramework from "./routes/working-framework.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/todos", todoRouter);
 app.use("/api/v1/pomodoros", pomodoroRouter);
 app.use("/api/v1/streaks", streakRouter);
 app.use("/api/v1/leaderboard", leaderboardRouter);
+app.use("/api/v1/working-framework", workingFramework);
 
 export default app;
