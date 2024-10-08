1d21eb10f2845bd002ef6947bdbe02a5279ed66d
Samyak Shah <samyakshah3008@gmail.com>
2024-10-08 23:36:52 +0530

    feat: habits router add to root

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import cron from "node-cron";
 
 const app = express();
 
@@ -10,18 +11,23 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import habitsRouter from "./routes/habits.routes.js";
 import leaderboardRouter from "./routes/leaderboard.routes.js";
 import pomodoroRouter from "./routes/pomodoro.routes.js";
 import streakRouter from "./routes/streak.routes.js";
 import todoRouter from "./routes/todo.routes.js";
 import userRouter from "./routes/user.routes.js";
-import workingFramework from "./routes/working-framework.routes.js";
+import workingFrameworkRouter from "./routes/working-framework.routes.js";
+import { sendHabitReminders } from "./utils/cronJobs.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/todos", todoRouter);
 app.use("/api/v1/pomodoros", pomodoroRouter);
 app.use("/api/v1/streaks", streakRouter);
 app.use("/api/v1/leaderboard", leaderboardRouter);
-app.use("/api/v1/working-framework", workingFramework);
+app.use("/api/v1/working-framework", workingFrameworkRouter);
+app.use("/api/v1/habits", habitsRouter);
+
+cron.schedule("* * * * *", sendHabitReminders);
 
 export default app;
