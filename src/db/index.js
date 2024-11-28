f2794184945ff5eadb7b19b236125a10b7f86d0d
Samyak Shah <samyakshah3008@gmail.com>
2024-11-28 19:59:55 +0530

    end not loading fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
